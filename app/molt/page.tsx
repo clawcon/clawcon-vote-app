10aa508f3f90496eb87e7009a4656a6fcf8d79a9
Michael Galpert <containsmsg@msgs-Mac-mini.local>
2026-02-04 14:51:32 -0800

    Add /molt page with screenshot image

---
+++ app/molt/page.tsx
@@ -0,0 +1,30 @@
+export const metadata = {
+  title: "Molt – ClawCon SF",
+};
+
+export default function MoltPage() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        background: "#000",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 0,
+        margin: 0,
+      }}
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/molt.png"
+        alt="Molt"
+        style={{
+          maxWidth: "100%",
+          maxHeight: "100vh",
+          objectFit: "contain",
+        }}
+      />
+    </div>
+  );
+}
